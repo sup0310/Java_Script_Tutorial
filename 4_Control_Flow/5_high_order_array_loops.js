c18d5e2b4fa87bb1bd3f9fd045c471589c743081
Supriy0 <supriyomandal310@gmail.com>
2026-08-30 01:36:33 +0530

    Filetr, Map & Reduce Methods

--- 4_Control_Flow/5_high_order_array_loops.js
+++ 4_Control_Flow/5_high_order_array_loops.js
@@ -71,6 +71,7 @@ console.log();
 coding.forEach((element, idx) => {
     console.log(idx + " > " + element);
 });
+console.log();
 
 
 codings = [
@@ -91,3 +92,93 @@ codings = [
 codings.forEach(code => {
     console.log(`The Shortcut of ${code.name} is ${code.fileName}`);
 });
+console.log();
+
+
+// FILTER
+// values = coding.forEach(function(item){
+//     console.log(item);
+//     return item;
+// });
+// console.log(values);
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const greater = numbers.filter(num => num > 5);
+console.log(greater);
+
+const great = numbers.filter(num => {return num > 4})
+console.log(great);
+
+const newNums = []
+numbers.forEach(num => {
+    if (num > 4){
+        newNums.push(num)
+    }
+})
+console.log(newNums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+const userBooks = books.filter(book => book.genre === 'Non-Fiction' && book.edition > 2000);
+console.log(userBooks);
+console.log();
+
+
+const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const added = mynums.map( num => num+10)
+console.log(added);
+console.log();
+
+
+// CHAINING
+const newnum = mynums.map(num => num * 10).map(num => num + 1).filter(num => num >= 50)
+console.log(newnum);
+console.log();
+
+
+// REDUCE
+// const initial = 0
+// const summ = mynums.reduce( (acc, val) => { console.log(acc, val);
+//  return acc + val}, 0)
+// console.log(summ);
+// console.log();
+
+const initial = 0
+const summ = mynums.reduce((acc, val) => acc + val, 0)
+console.log(summ);
+console.log();
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const total_bill = shoppingCart.reduce((acc, item) => (acc+item.price), 0);
+console.log(total_bill);
